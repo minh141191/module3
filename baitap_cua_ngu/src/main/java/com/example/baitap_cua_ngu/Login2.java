4e7583b7e8cce3dd3b8649ca3c973dd0f75bb1b5
Minh Tran <minh.tv1411@gmail.com>
2023-05-29 17:05:32 +0700

    update day 29-05

---
+++ baitap_cua_ngu/src/main/java/com/example/baitap_cua_ngu/Login2.java
@@ -0,0 +1,18 @@
+package com.example.baitap_cua_ngu;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Login2", value = "/Login2")
+public class Login2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
